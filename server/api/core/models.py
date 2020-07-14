4dc4df861d0ebc73051643a3e4e125b11490fd4d
guilherme <guilherme@guilhermeweb.dev>
2020-07-14 02:39:03 -0300

    Migração

--- server/api/core/models.py
+++ server/api/core/models.py
@@ -16,18 +16,25 @@ class Item(models.Model):
         max_length=300
     )
     image = models.ImageField(
-        upload_to='media/images/itens'
+        upload_to='media/images/items'
     )
-    owner = models.ForeignKey(
-        get_user_model(),
+    store = models.ForeignKey(
+        'Store',
         on_delete=models.CASCADE,
     )
-    reference = models.CharField(
+    reference = models.SlugField(
         max_length=50,
     )
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def url(self):
+        return f'/{self.store.get_url()}/{self.reference}'
+
     class Meta:
-        unique_together = ('reference', 'owner')
+        unique_together = ('reference', 'store')
 
 
 class Store(models.Model):
@@ -42,6 +49,7 @@ class Store(models.Model):
     )
     slug = models.SlugField(
         max_length=150,
+        unique=True,
     )
     background_color = models.CharField(
         max_length=15,
@@ -80,3 +88,10 @@ class Store(models.Model):
         null=True,
         blank=True,
     )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def url():
+        return f'/{self.slug}'
