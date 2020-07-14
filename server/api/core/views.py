f0119a3a34b9fab32e4128a70e4c87720fc49d73
guilherme <guilherme@guilhermeweb.dev>
2020-07-14 01:45:46 -0300

    pequenos ajustes

--- server/api/core/views.py
+++ server/api/core/views.py
@@ -7,32 +7,38 @@ class LoginView(FormView):
     template_name = 'login.html'
     form_class = forms.LoginForm
 
-    def get_success_url(self):
+    def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return self.request.GET.get('next', '/')
-        return '/login/'
+            return redirect(self.request.GET.get('next', '/'))
+        return super().get(*args, **kwargs)
+
+    def get_success_url(self):
+        return self.request.GET.get('next', '/')
     
     def form_valid(self, form):
         data = form.cleaned_data
+        print(data)
         user = authenticate(
-            email=data.get('email', data.get('username')),
+            username=data.get('email', data.get('username')),
             password=data['password']
         )
+        print(user)
         return super().form_valid(form)
 
 
 class RegistrationView(FormView):
     template_name = 'registration.html'
     form_class = forms.RegisterForm
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(self.request.GET.get('next', '/'))
+        return super().get(*args, **kwargs)
     
     def get_success_url(self):
         if self.request.user.is_authenticated:
             return self.request.GET.get('next', '/')
         return '/registration/'
-
-    def form_invalid(self, form):
-        print(form.non_field_errors())
-        return super().form_invalid(form)
 
     def form_valid(self, form):
         user = form.save()
