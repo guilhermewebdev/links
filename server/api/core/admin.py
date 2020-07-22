261b72b61212de7c38d3b03bf6f70c545d1771f1
guilherme <guilherme@guilhermeweb.dev>
2020-07-22 00:45:57 -0300

    Filtragem do django admin

--- server/api/core/admin.py
+++ server/api/core/admin.py
@@ -1,27 +1,45 @@
 from django.contrib import admin
 from . import models
 
+class SetOwner(admin.ModelAdmin):
+    exclude = ('owner',)
+    def save_model(self, request, obj, *args, **kwargs):
+        obj.owner = request.user
+        super().save_model(request, obj, *args, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
 class ItemInline(admin.StackedInline):
     model = models.Item
     extra = 1
     ordering = ('index',)
 
 @admin.register(models.Store)
-class StoreAdmin(admin.ModelAdmin):
+class StoreAdmin(SetOwner):
     inlines = [
         ItemInline,
     ]
 
 
 @admin.register(models.ItemTheme)
-class ItemThemeAdmin(admin.ModelAdmin):
+class ItemThemeAdmin(SetOwner):
     pass
 
 class ItemThemeInline(admin.StackedInline):
     model = models.ItemTheme
+    exclude = ('owner',)
+    def save_model(self, request, obj, *args, **kwargs):
+        obj.owner = request.user
+        super().save_model(request, obj, *args, **kwargs)
 
 @admin.register(models.Theme)
-class ThemeAdmin(admin.ModelAdmin):
+class ThemeAdmin(SetOwner):
     inlines = [
         ItemThemeInline
     ]
+
+    
